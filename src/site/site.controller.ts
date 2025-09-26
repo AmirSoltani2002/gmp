344ea425a06b663d06bd5951a17cdd11ea053ebe
amirabbas <amirsoltani1381@gmail.com>
2025-09-26 12:15:36 +0330

    add site endpoints

--- src/site/site.controller.ts
+++ src/site/site.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, BadRequestException, Req } from '@nestjs/common';
 import { SiteService } from './site.service';
 import { CreateSiteDto } from './dto/create-site.dto';
 import { UpdateSiteDto } from './dto/update-site.dto';
@@ -10,12 +10,15 @@ export class SiteController {
   constructor(private readonly siteService: SiteService) {}
 
   @Post()
-  create(@Body() CreateSiteDto: CreateSiteDto) {
-    return this.siteService.create(CreateSiteDto);
+  @Roles([ROLES.SYSTEM, ROLES.IFDAUSER, ROLES.IFDAMANAGER, ROLES.QRP])
+  create(@Body() createSiteDto: CreateSiteDto, @Request() req) {
+    if(req['user'].role === ROLES.QRP && createSiteDto.companyId != req['user'].currentCompanyId)
+      throw new BadRequestException('The QRP User cannot Edit this Site');
+    return this.siteService.create(createSiteDto);
   }
 
   @Get()
-  @Roles([ROLES.SYSTEM, ROLES.IFDAUSER, ROLES.IFDAMANAGER])
+  @Roles([ROLES.SYSTEM, ROLES.IFDAUSER, ROLES.IFDAMANAGER, ROLES.QRP])
   findAll() {
     return this.siteService.findAll();
   }
@@ -27,17 +30,19 @@ export class SiteController {
   }
 
   @Patch(':id')
-  @Roles([ROLES.SYSTEM, ROLES.IFDAMANAGER])
+  @Roles([ROLES.SYSTEM, ROLES.IFDAMANAGER, ROLES.QRP, ROLES.IFDAUSER])
   async update(@Param('id') id: string, @Body() UpdateSiteDto: UpdateSiteDto, @Request() req) {
     const thisSite = await this.siteService.findOne(+id);
-    if(req['user'].role === ROLES.QRP && thisSite.companyId != req['user'].currentCompanyId)
+    if([ROLES.QRP, ROLES.IFDAUSER].includes(req['user'].role) && thisSite.companyId != req['user'].currentCompanyId)
       throw new BadRequestException('The QRP User cannot Edit this Site');
     return this.siteService.update(+id, UpdateSiteDto);
   }
 
   @Delete(':id')
-  @Roles([ROLES.SYSTEM])
+  @Roles([ROLES.SYSTEM, ROLES.IFDAMANAGER, ROLES.QRP, ROLES.IFDAUSER])
   remove(@Param('id') id: string) {
+    if([ROLES.QRP, ROLES.IFDAUSER].includes(req['user'].role) && thisSite.companyId != req['user'].currentCompanyId)
+      throw new BadRequestException('The QRP User cannot delete this Site');
     return this.siteService.remove(+id);
   }
 }
