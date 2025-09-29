067aa9c19e0fe5a995dec6dc7c8fb00abfd6e785
alia-asl <aliabdollahiasl@gmail.com>
2025-09-29 13:07:09 +0330

    ensure numbers

--- src/line/dto/create-line.dto.ts
+++ src/line/dto/create-line.dto.ts
@@ -1,32 +1,46 @@
 import { IsBoolean, IsDateString, IsNumber, IsOptional, IsString } from "class-validator";
+import { Type } from "class-transformer";
 
 export class CreateLineDto {
     @IsNumber()
+    @Type(() => Number)
     siteId: number;
+    
     @IsOptional()
     @IsString()
     nameEn?: string;
+    
+    @IsOptional()
     @IsBoolean()
-    @IsOptional()
     isActive?: boolean;
+    
     @IsOptional()
     @IsNumber()
+    @Type(() => Number)
     capacity?: number;
+    
     @IsOptional()
     @IsNumber()
+    @Type(() => Number)
     OEB?: number;
+    
     @IsOptional()
     @IsString()
     nameFa?: string;
+    
+    @IsOptional()
     @IsBoolean()
-    @IsOptional()
     isSterile?: boolean;
-    @IsNumber()
+    
     @IsOptional()
+    @IsNumber()
+    @Type(() => Number)
     actual?: number;
+    
     @IsOptional()
     @IsString()
     startFrom?: string;
+    
     @IsOptional()
     @IsDateString()
     opensDate?: string;
