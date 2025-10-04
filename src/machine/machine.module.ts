0e722b5d5d6a4d51d6389b7370f7df06891dd2b1
alia-asl <aliabdollahiasl@gmail.com>
2025-10-04 15:02:04 +0330

    import line & site in machine

--- src/machine/machine.module.ts
+++ src/machine/machine.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MachineService } from './machine.service';
 import { MachineController } from './machine.controller';
 import { MachineTypeModule } from 'src/machine-type/machine-type.module';
+import { SiteModule } from 'src/site/site.module';
+import { LineModule } from 'src/line/line.module';
 
 @Module({
   controllers: [MachineController],
   providers: [MachineService],
-  imports: [MachineTypeModule],
+  imports: [MachineTypeModule, SiteModule, LineModule],
   exports: [MachineService],
 })
 export class MachineModule {}
